b594c4acf6eb29a5d586bd1658b3506b4bff6eb6
Park Billy <rtlink.park@gmail.com>
2020-03-01 11:09:08 +0900

    add bundle script

--- mobx/store.js
+++ mobx/store.js
@@ -5,14 +5,10 @@ class CounterStore {
 
 	increment() {
 		this.counter++;
-		console.log("increment", this.counter);
 	}
 
 	decrement() {
 		this.counter--;
-		console.log("decrement", this.counter);
 	}
-
 }
-
 export default new CounterStore();
